ebf4565cc2deff6acb4704cfdaa5a632cb5aa25d
Alex Beazley <alexbeazley18@outlook.com>
2022-04-11 06:24:33 +1000

    mobile option 1: Expo

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,7 +18,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
+// Use static Angular files
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseAuthorization();
 

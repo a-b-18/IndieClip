d188b21a74b6488bf4978384b7f1c7f66d006bcc
Alex Beazley <alexbeazley18@outlook.com>
2022-04-17 20:42:00 +1000

    Final struct init.

--- client/android/app/src/main/kotlin/com/indieclip/client/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.indieclip.client
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ client_mobile/android/app/src/main/kotlin/com/indieclip/client_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.indieclip.client_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
